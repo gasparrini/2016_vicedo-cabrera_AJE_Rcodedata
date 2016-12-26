6adbcef11ad84cdd7436733aca31f4a84164e71f
Antonio Gasparrini <gaspanero@hotmail.com>
2016-12-27 00:34:10 +0100

    Reformat the code

--- 02.results.R
+++ 02.results.R
@@ -17,44 +17,44 @@
 ## RR ESTIMATES OF THE DIFFERENT TEMPERATURE INDICATORS
 
 # RR OF MEAN DAILY TEMPERATURE (AT 1 AND 99 PERCENTILES)
-RR.h <- unlist(crosspred(cb,model.upd,at=quantile(data$tmean,0.99,
-  na.rm=T),cen=mmt)[c("allRRfit","allRRlow","allRRhigh")])
-RR.c <- unlist(crosspred(cb,model.upd,at=quantile(data$tmean,0.01,
-  na.rm=T),cen=mmt)[c("allRRfit","allRRlow","allRRhigh")])
+RR.h <- unlist(crosspred(cb, model.upd, at = quantile(data$tmean, 0.99,
+  na.rm = T), cen = mmt)[c("allRRfit", "allRRlow", "allRRhigh")])
+RR.c <- unlist(crosspred(cb, model.upd, at = quantile(data$tmean, 0.01,
+  na.rm = T), cen = mmt)[c("allRRfit", "allRRlow", "allRRhigh")])
 
 # BETWEEN-DAY INCREASE IN TEMPERATURE (bdinc)
 beta <- coef(model.upd)["bdinc"]
-var <- vcov(model.upd)["bdinc","bdinc"]
-median <- median(bdinc[bdinc>0],na.rm=T)
-RR.bdinc <- rbind(c(round(exp(beta*median),3),
-  round(exp((beta+c(-1,1)*1.96*sqrt(var))*median),3)))
+var <- vcov(model.upd)["bdinc", "bdinc"]
+median <- median(bdinc[bdinc > 0], na.rm = T)
+RR.bdinc <- rbind(c(round(exp(beta * median), 3),
+  round(exp((beta + c(-1, 1) * 1.96 * sqrt(var)) * median), 3)))
 
 # BETWEEN-DAY DECREASE IN TEMPERATURE (bddec)
 beta <- coef(model.upd)["bddec"]
-var <- vcov(model.upd)["bddec","bddec"]
-median <- median(bddec[bddec>0],na.rm=T)
-RR.bddec <- rbind(c(round(exp(beta*median),3),
-  round(exp((beta+c(-1,1)*1.96*sqrt(var))*median),3)))
+var <- vcov(model.upd)["bddec", "bddec"]
+median <- median(bddec[bddec > 0], na.rm = T)
+RR.bddec <- rbind(c(round(exp(beta * median), 3),
+  round(exp((beta + c(-1, 1) * 1.96 * sqrt(var)) * median), 3)))
 
 # WITHIN-DAY CHANGE IN TEMPERATURE IN HOT DAYS (wdhot)
 beta <- coef(model.upd)["wdhot"]
-var <- vcov(model.upd)["wdhot","wdhot"]
-median <- median(wdhot[wdhot>0],na.rm=T)
-RR.wdhot <- rbind(c(round(exp(beta*median),3),
-  round(exp((beta+c(-1,1)*1.96*sqrt(var))*median),3)))
+var <- vcov(model.upd)["wdhot", "wdhot"]
+median <- median(wdhot[wdhot > 0], na.rm = T)
+RR.wdhot <- rbind(c(round(exp(beta * median), 3),
+  round(exp((beta + c(-1, 1) * 1.96 * sqrt(var)) * median), 3)))
 
 # WITHIN-DAY CHANGE IN TEMPERATURE IN COLD DAYS (wdcold)
 beta <- coef(model.upd)["wdcold"]
-var <- vcov(model.upd)["wdcold","wdcold"]
-median <- median(wdcold[wdcold>0],na.rm=T)
-RR.wdcold <- rbind(c(round(exp(beta*median),3),
-  round(exp((beta+c(-1,1)*1.96*sqrt(var))*median),3)))
+var <- vcov(model.upd)["wdcold", "wdcold"]
+median <- median(wdcold[wdcold > 0], na.rm = T)
+RR.wdcold <- rbind(c(round(exp(beta * median), 3),
+  round(exp((beta + c(-1, 1) * 1.96 * sqrt(var)) * median), 3)))
 
 # GENERATE THE TABLE
-RR.table <- rbind(RR.h,RR.c,RR.bdinc,RR.bddec,RR.wdhot,RR.wdcold)
-rownames(RR.table) <- c("RR.heat","RR.cold","RR.bdinc","RR.bddec",
-  "RR.wdhot","RR.wdcold")
-colnames(RR.table) <- c("RR","RR.low","RR.high")
-formatC(RR.table,format="f",digits=3)
+RR.table <- rbind(RR.h, RR.c, RR.bdinc, RR.bddec, RR.wdhot, RR.wdcold)
+rownames(RR.table) <- c("RR.heat", "RR.cold", "RR.bdinc", "RR.bddec",
+  "RR.wdhot", "RR.wdcold")
+colnames(RR.table) <- c("RR", "RR.low", "RR.high")
+formatC(RR.table, format = "f", digits = 3)
 
 #
--- 03.plots.R
+++ 03.plots.R
@@ -17,18 +17,20 @@
 ################################################################################
 ### WEB FIGURE 1a
 
-d3 <- plot(pred.l,main="Exposure-lag-response",xlab="Temperature (°C)",zlab="RR",
-  phi=35,theta=220,ltheta=170,shade=0.1,cex.axis=0.8,cex.lab=0.8)
-mtext("London 1993-2006",cex=0.8)
-mmt <- pred.l$predvar[pred.l$allRRfit==1]
-lines(trans3d(x=mmt,y=0:21,z=pred.l$matRRfit[as.character(mmt),],
-  pmat=d3),lwd=2, col="red")
+d3 <- plot(pred.l, main = "Exposure-lag-response", xlab = "Temperature (?C)",
+  zlab = "RR", phi = 35, theta = 220, ltheta = 170, shade = 0.1, cex.axis = 0.8,
+  cex.lab = 0.8)
+mtext("London 1993-2006", cex = 0.8)
+mmt <- pred.l$predvar[pred.l$allRRfit == 1]
+lines(trans3d(x = mmt, y = 0:21, z = pred.l$matRRfit[as.character(mmt), ],
+  pmat = d3), lwd = 2, col = "red")
 
 ################################################################################
 ### WEB FIGURE 1b
 
-plot(pred.l,"overall", xlab="Temperature (°C)",ylab="RR",cex.axis=0.9,
-  ylim=c(0.5,max(pred.l$allRRhigh)),main="Overall cumulative exposure-response")
-mtext("London 1993-2006",cex=0.8)
+plot(pred.l, "overall", xlab = "Temperature (?C)", ylab = "RR", cex.axis = 0.9,
+  ylim = c(0.5, max(pred.l$allRRhigh)),
+  main = "Overall cumulative exposure-response")
+mtext("London 1993-2006", cex=0.8)
 
 #
